be458bc944cb6b20cabacf1b2afe7d9911880474
Reza <rezarendra29@gmail.com>
2025-05-20 19:08:00 +0700

    pp

--- KasihkuPBO/Form1.cs
+++ KasihkuPBO/Form1.cs
@@ -1,3 +1,15 @@
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
 namespace KasihkuPBO
 {
     public partial class Form1 : Form
@@ -19,7 +31,40 @@ namespace KasihkuPBO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Button berhasil diklik!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            string connString = "Host=localhost;Port=5432;Username=postgres;Password=fahrezaadam1784;Database=KASIHKU;";
+            string usernameInput = LoginUsername.Text.Trim();
+            string passwordInput = LoginPassword.Text.Trim();
+
+            try
+            {
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+                    string sql = "SELECT COUNT(*) FROM akun WHERE username = @user AND kata_sandi = @pass";
+                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("user", usernameInput);
+                        cmd.Parameters.AddWithValue("pass", passwordInput);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("Login berhasil!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // TODO: buka form selanjutnya
+                        }
+                        else
+                        {
+                            MessageBox.Show("Login gagal. Username atau password salah.", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
         }
+
     }
 }
